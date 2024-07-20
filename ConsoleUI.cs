671030a8d9a291127b2deb8de5439366dbcfa500
Niels Stockfleth <nyls@aol.com>
2024-07-20 13:39:49 +0200

    Do not upload artifacts

--- ConsoleUI.cs
+++ ConsoleUI.cs
@@ -294,7 +294,7 @@ namespace PasswordManagerConsole
 
         private void ShowHelpCommand()
         {
-            Console.WriteLine("Myna Password Manager Console version 8.0.1");
+            Console.WriteLine("Myna Password Manager Console version 8.0.2");
             Console.WriteLine("Copyright (c) 2024 Niels Stockfleth. All rights reserved.");
             Console.WriteLine();
             Console.WriteLine("Commands:");
